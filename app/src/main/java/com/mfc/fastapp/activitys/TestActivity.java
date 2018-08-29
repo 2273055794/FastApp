7c511af84c0c52c86484550789d0a5ed3fcdfa2e
meng <2273055794@qq.com>
2018-08-29 10:28:02 +0800

    项目克隆后提交

---
+++ app/src/main/java/com/mfc/fastapp/activitys/TestActivity.java
@@ -0,0 +1,12 @@
+package com.mfc.fastapp.activitys;
+
+import com.mfc.fastapp.R;
+import com.mfc.fastapp.base.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_test;
+    }
+}
